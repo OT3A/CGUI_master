2edb00a96aa3c69296270c3bc9d92a4263df6b6c
Ahmed-0-Ibrahim <ahmed20191700059@cis.asu.edu.eg>
2022-12-04 19:11:53 +0200

    mesh fakr

--- CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
+++ CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
@@ -9,16 +9,29 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class JarvisMarch : Algorithm
     {
+        public double Theta(Point v1,Point v2,double m1,double m2)
+        {
+            double dotProduct = v1.X * v2.X + v1.Y * v2.Y;
+            double tmp = dotProduct / (m1 * m2);
+            double theta = Math.Acos(tmp);
+            return theta;
+
+        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            double dotProduct;
+            Point start, end;
+            Point V1, V2;
+            double magV1, magV2;
             List<Point> right = new List<Point>();
             List<Point> left = new List<Point>();
             List<Point> extremPoints = new List<Point>();
             Point last;
             int size = points.Count();
             points.Sort((p1, p2) => p1.X.CompareTo(p2.X));
-            last = points[size - 1];
+            last = points[0];
             extremPoints.Add(last);
+            bool second = false;
             for (int i = 0; i < size; i++)
             {
                 if (last != points[i] && !(extremPoints.Contains(points[i])))
@@ -40,6 +53,50 @@ namespace CGAlgorithms.Algorithms.ConvexHull
                             {
                                 right.Add(points[j]);
                             }
+                        }
+                    }
+                    if (left.Count() == 0 || right.Count() == 0)
+                    {
+                        if (!extremPoints.Contains(points[i]))
+                        {
+                            extremPoints.Add(points[i]);
+                            last = points[i];
+                            second = true;
+                            break;
+                        }
+                    }
+                    right.Clear();
+                    left.Clear();
+                }
+                right.Clear();
+                left.Clear();
+                if (second)
+                {
+                    break;
+                }
+            }
+            end = extremPoints[0];
+            start = extremPoints[1];
+            for (int i = 0; i < size; i++)
+            {
+                double maxTheata = 0;
+                if (last != points[i] && !(extremPoints.Contains(points[i])))
+                {
+
+                    Line s = new Line(last, points[i]);
+                    V1 = CGUtilities.HelperMethods.GetVector(s);
+                    double tmp = (V1.X * V1.X) + (V1.Y + V1.Y);
+                    magV1 = Math.Sqrt(tmp);
+                    for (int j = 0; j < size; j++)
+                    {
+                        Line l = new Line( points[j], points[i]);
+                        V2 = CGUtilities.HelperMethods.GetVector(l);
+                        double tmp2 = (V2.X * V2.X) + (V2.Y + V2.Y);
+                        magV2 = Math.Sqrt(tmp2);
+
+                        if (points[j] != points[i] && points[j] != last)
+                        {
+                            
                         }
                     }
                     if (left.Count() == 0 || right.Count() == 0)
--- CGAlgorithms/Algorithms/ConvexHull/QuickHull.cs
+++ CGAlgorithms/Algorithms/ConvexHull/QuickHull.cs
@@ -9,28 +9,115 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class QuickHull : Algorithm
     {
+        public double distance(Line l,Point p)
+        {
+            double d = 0;
+            double d1 = Math.Sqrt(Math.Pow(p.Y - l.Start.Y, 2) + Math.Pow(p.X - l.Start.X, 2));
+            double d2 = Math.Sqrt(Math.Pow(p.Y - l.End.Y, 2) + Math.Pow(p.X - l.End.X, 2));
+            d = d1 + d2;
+            return d;
+        } 
+        public void Recursion(Line l1,Line l2 ,List<Point> input, List<Point> output)
+        {
+            Point p1 =null;
+            Point p2 =null;
+            double maxD1 = 0;
+            double maxD2 = 0;
+            int size = input.Count();
+            for(int i = 0; i < size; i++)
+            {
+                if(CGUtilities.HelperMethods.CheckTurn(l1, input[i]) == CGUtilities.Enums.TurnType.Right)
+                {
+                    double d = distance(l1, input[i]);
+                    if (d > maxD1)
+                    {
+                        p1 = input[i];
+                        maxD1 = d;
+                    }
+                }
+            }
+            if (p1 != null)
+            {
+                output.Add(p1);
+                Line ll1 = new Line(l1.Start, p1);
+                Line ll2 = new Line(p1, l1.End);
+                Recursion(ll1, ll2, input, output);
+            }
+            
+            
+            for (int i = 0; i < size; i++)
+            {
+
+                if (CGUtilities.HelperMethods.CheckTurn(l2, input[i]) == CGUtilities.Enums.TurnType.Right)
+                {
+                    double d = distance(l2, input[i]);
+                    if (d > maxD2)
+                    {
+                        p2 = input[i];
+                        maxD2 = d;
+                    }
+                }
+            }
+            if (p2 != null)
+            {
+                output.Add(p2);
+                Line ll1 = new Line(l2.Start, p2);
+                Line ll2 = new Line(p2, l2.End);
+                Recursion(ll1, ll2, input, output);
+            }
+        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
             int size = points.Count();
-            List<Point> copePoints = new List<Point>();
-            Point minX;
-            Point miny;
-            Point maxX;
-            Point maxy;
-            for(int i = 0; i < size; i++)
-            {
-                Point p = new Point(0, 0);
-                p.X = points[i].X;
-                p.Y = points[i].Y;
-                copePoints.Add(p);
-            }
-            copePoints.Sort((p1, p2) => p1.X.CompareTo(p2.X));
-            minX = copePoints[0];
-            maxX = copePoints[size - 1];
-            copePoints.Sort((p1, p2) => p1.Y.CompareTo(p2.Y));
-            miny = copePoints[0];
-            maxy = copePoints[size - 1];
+            Point minX = null;
+            Point miny = null;
+            Point maxX = null;
+            Point maxy = null;
 
+            for (int i = 0; i < size; i++)
+            {
+                if(i != 0)
+                {
+                    if (points[i].X < minX.X)
+                    {
+                        minX = points[i];
+                    }
+                    else if (points[i].X > maxX.X)
+                    {
+                        maxX = points[i];
+                    }
+                    else if (points[i].Y < miny.Y)
+                    {
+                        miny = points[i];
+                    }
+                    else if (points[i].Y > maxy.Y)
+                    {
+                        maxy = points[i];
+                    }
+                }
+                else
+                {
+                    minX = points[i];
+                    miny = points[i];
+                    maxX = points[i];
+                    maxy = points[i];
+                }
+            }
+
+            if(!outPoints.Contains(miny))
+                outPoints.Add(miny);
+            if (!outPoints.Contains(minX) )
+                outPoints.Add(minX);
+            if (!outPoints.Contains(maxy))
+                outPoints.Add(maxy);
+            if (!outPoints.Contains(maxX))
+                outPoints.Add(maxX);
+            Line l1 = new Line(miny, maxX);
+            Line l2 = new Line(maxX, maxy);
+            Line l3 = new Line(maxy, minX);
+            Line l4 = new Line(minX, miny);
+            Recursion(l1, l2, points, outPoints);
+            Recursion(l3, l4, points, outPoints);
         }
 
         public override string ToString()
